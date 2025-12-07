432166abf11eb8e1372d8ba71897ef59c9987b34
Pranshul Thakur <Pranshul7thakur10@gmail.com>
2025-12-07 17:24:31 +0530

    Initial basic HTML structure with layout placeholders

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("startBtn").onclick = () => alert("Start clicked");
+document.getElementById("pauseBtn").onclick = () => alert("Pause clicked");
+document.getElementById("resetBtn").onclick = () => alert("Reset clicked");
